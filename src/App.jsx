7894a48d296fba80b0bf709d393e865bd59d103b
Akash Tirkey <akash.tirkeywork@gmail.com>
2026-02-11 20:27:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Pagination from './Pagination'
+
+function App() {
+  
+
+  return (
+    <>
+     <Pagination/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pagination.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import styles from './styles/Pagination.module.css'
+
+const Pagination = () =>{
+//state
+const[data,setData] = useState([]);
+const[loading,setLoading] = useState(true);
+const[pgCount,setPgCount]=useState(1);
+
+const itemsPerPage = 10;
+
+
+useEffect(()=>{
+    const fetchData = async() =>{
+        try {
+            const response = await axios.get("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
+            setData(response.data)
+            console.log("response:",response)
+        } catch (error) {
+            console.error("Couldn't fetch the Data",error);
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    fetchData();
+},[])
+
+const totalPages = Math.ceil(data.length / itemsPerPage);
+
+const startIndex = (pgCount -1)* itemsPerPage;
+const currentData = data.slice(
+    startIndex,
+    startIndex + itemsPerPage
+)
+
+if(loading) {
+    return <p>Loading Data...</p>
+}
+
+const handleNext = () =>{
+    if(pgCount < totalPages){
+        setPgCount((prev) => prev+1);
+    }
+}
+
+const handlePrev = ()=>{
+    if(pgCount > 1){
+        setPgCount(prev => prev-1);
+    }
+}
+
+    return(
+        <>
+        <h1>Employee Data Table</h1>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+            </tr>
+
+            <tbody>
+            {currentData.map((item) =>(
+             <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.role}</td>
+             </tr>   
+            ))}
+            </tbody>
+            
+        </table>
+        <div className={styles.btn}>
+        <button disabled={pgCount === 1} onClick={handlePrev}>Previous</button>
+            <button>{pgCount}</button>
+            <button disabled = {pgCount === totalPages} onClick={handleNext}>Next</button> 
+            </div>
+        </>
+    )
+}
+
+export default Pagination;
